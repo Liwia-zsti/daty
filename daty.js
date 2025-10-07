6db572e2bfd868fbbfc2a36a07499f20cc26c7cd
Liwia-zsti <doszti123@gmail.com>
2025-10-07 10:12:47 +0200

    Update daty.js

--- daty.js
+++ daty.js
@@ -1,9 +1,10 @@
-const selectedDayInp = document.querySelector('#selectedDay');
-const display = document.querySelector('section');
+const selectedDayInp1 = document.querySelector('#selectedDay1');
+const display1 = document.querySelector('section');
 
-selectedDayInp.addEventListener('change',function() {
-    const selectedDay = new Date(selectedDayInp.value);
-    display.innerHTML = selectedDay.toLocaleDateString
+
+selectedDayInp1.addEventListener('change',function() {
+    const selectedDay1 = new Date(selectedDayInp1.value);
+    display1.innerHTML = selectedDay1.toLocaleDateString
     ('pl-PL',{
         weekday: "long",
         day: '2-digit',
@@ -11,3 +12,40 @@ selectedDayInp.addEventListener('change',function() {
         year: 'numeric',
     });
 });
+const selectedDayInp2 = document.querySelector('#selectedDay2');
+const display2 = document.querySelector('section');
+
+selectedDayInp2.addEventListener('change',function() {
+    const selectedDay2 = new Date(selectedDayInp2.value);
+    display2.innerHTML = selectedDay2.toLocaleDateString
+    ('pl-PL',{
+        weekday: "long",
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+    });
+});
+if (selectedDay1.getTime()>selectedDay2.getTime()) {
+const data1 = selectedDay2;
+const data2 = selectedDay1;
+const miliseconds1 = data1.getTime();
+const miliseconds2 = data2.getTime();
+roznica = miliseconds1 = miliseconds2;
+document.getElementById('wynik').innerHTML('Różnica dni wynosi:' + roznica) / (1000*24*60*60);
+}
+else if (selectedDay1.getTime()<selectedDay2.getTime()) {
+    const data1 = selectedDay2;
+    const data2 = selectedDay1;
+    const miliseconds1 = data1.getTime();
+    const miliseconds2 = data2.getTime();
+    roznica = miliseconds1 = miliseconds2;
+    document.getElementById('wynik').innerHTML('Różnica dni wynosi:' + roznica) / (1000*24*60*60);
+    }
+    if (selectedDay1.getTime()=selectedDay2.getTime()) {
+        const data1 = selectedDay2;
+        const data2 = selectedDay1;
+        const miliseconds1 = data1.getTime();
+        const miliseconds2 = data2.getTime();
+        roznica = miliseconds1 = miliseconds2;
+        document.getElementById('wynik').innerHTML('Daty są takie same' + roznica) / (1000*24*60*60);
+        }
