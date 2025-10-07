63a92f349cfe9b353bcdb70e0478e5633a6eac54
Liwia-zsti <doszti123@gmail.com>
2025-10-07 09:03:45 +0200

    Update daty.js

--- daty.js
+++ daty.js
@@ -1,3 +1,8 @@
-const data = new Date(Date.UTC)
-console.log(data)
-document.querySelector('wynik').innerHTML = data
+const data = new Date(Date.UTC);
+console.log(data);
+
+data.toDateString;
+console.log(data);
+
+
+document.querySelector('wynik').innerHTML = data;
